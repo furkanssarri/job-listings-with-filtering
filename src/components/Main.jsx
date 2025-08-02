dbc8db58260b39bf2a8d3bdd7bdc9d4a7bcf117f
Furkan Sarı <furkansa@hotmail.com.tr>
2025-08-03 00:35:26 +0300

    style: implement basic mobile layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import data from "../data/data.json";
 
 const Main = () => {
+  useEffect(() => {
+    console.log(data[0]);
+  }, []);
   return (
     <main>
       {data &&
         data.map((item) => (
-          <article key={item.id}>
+          <article
+            key={item.id}
+            className={item.featured ? "featured-left-border" : ""}
+          >
             <div className="job-info">
               <img
                 className="company-logo"
@@ -14,21 +21,43 @@ const Main = () => {
               />
               <div className="job-details">
                 <div className="job-title-container">
-                  <h2>{item.company}</h2>
-                  <div className="job-tags-container">
-                    {item.new && <span>New!</span>}
-                    {item.featured && <span>Featured</span>}
+                  <h1 className="text-preset-4-medium">{item.company}</h1>
+                  <div className="job-tags-container text-preset-4-medium">
+                    {item.new && <span className="tag new">New!</span>}
+                    {item.featured && (
+                      <span className="tag featured">Featured</span>
+                    )}
                   </div>
                 </div>
-                <h1>{item.position}</h1>
+                <h2>{item.position}</h2>
 
                 <div className="job-meta-container">
-                  <ul>
+                  <ul className="text-preset-3-medium">
                     <li>{item.postedAt}</li>
                     <li>{item.contract}</li>
                     <li>{item.location}</li>
                   </ul>
                 </div>
+              </div>
+              <div className="job-tags text-preset-3-bold">
+                <span>
+                  <a href="#">{item.role}</a>
+                </span>
+                <span>
+                  <a href="#">{item.level}</a>
+                </span>
+                {item.languages &&
+                  item.languages.map((language) => (
+                    <span key={language}>
+                      <a href="#">{language}</a>
+                    </span>
+                  ))}
+                {item.tools &&
+                  item.tools.map((tool) => (
+                    <span key={tool}>
+                      <a href="#">{tool}</a>
+                    </span>
+                  ))}
               </div>
             </div>
           </article>
